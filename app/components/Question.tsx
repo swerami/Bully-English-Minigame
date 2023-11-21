d471c380a82d1f9e13dce62784b5defcfd604d24
TechRami <techrami@outlook.com>
2023-11-21 19:05:25 +0300

    create initial components, install font

---
+++ app/components/Question.tsx
@@ -0,0 +1,8 @@
+
+const Question = () => {
+  return <div className="flex flex-col gap-4">
+    <span>A N Y C O L</span>
+  </div>;
+};
+
+export default Question;
---
+++ app/components/Timer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timer = () => {
+  return <div>3:00</div>;
+};
+
+export default Timer;
